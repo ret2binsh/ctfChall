0db9bdd22603c9769dd440191d26dc2d7f63e6cb
Josh <joshua.b.gonzales>
2019-09-09 22:49:37 -0500

    Completed rebuilding double.c and added the flag.txt file

--- double.c
+++ double.c
@@ -25,6 +25,7 @@ void clear_buffer(){
 
 void menu(){
 	printf("**********************\n");
+	printf("         Menu\n");
 	printf("1.) CTF User Login\n");
 	printf("2.) CTF Team Login\n");
 	printf("3.) Display Team\n");
@@ -33,16 +34,100 @@ void menu(){
 	printf("6.) Exit\n\n");
 }
 
+// receive username and password and return pointer to struct
 struct ctfuser * get_user(struct ctfuser *user){
+
+	user = (struct ctfuser *)malloc(sizeof(struct ctfuser));
+	int bytes = 0;
+
+	printf("Enter username: ");
+	fflush(stdout);
+	// ensure we receive null terminate the string and burn the excess input buffer
+	bytes = read(0, user->name, USER_MAX);
+	if (bytes == USER_MAX && user->name[USER_MAX-1] != '\n'){
+
+		clear_buffer();
+		user->name[USER_MAX-1] = '\0';
+	}else if (bytes == USER_MAX){
+
+		user->name[USER_MAX-1] = '\0';
+	}else{
+
+		user->name[bytes-1] = '\0';
+	}
+
+	printf("Password: ");
+	fflush(stdout);
+	// ensure we receive null terminate the string and burn the excess input buffer
+	bytes = read(0, user->pass, USER_MAX);
+	if (bytes == USER_MAX && user->pass[USER_MAX-1] != '\n'){
+
+		clear_buffer();
+		user->pass[USER_MAX-1] = '\0';
+	}else if (bytes == USER_MAX){
+
+		user->pass[USER_MAX-1] = '\0';
+	}else{
+
+		user->pass[bytes-1] = '\0';
+	}
+
+	printf("\nWelcome %s\n", user->name);
+
+	fflush(stdout);
+	sleep(1);
 
 	return user;
 }
 
 struct ctfteam * get_team(struct ctfteam *team){
 
+	team = (struct ctfteam *)malloc(sizeof(struct ctfteam));
+	int bytes = 0;
+
+	printf("Enter team name: ");
+	fflush(stdout);	
+	// ensure we receive null terminate the string and burn the excess input buffer
+	bytes = read(0, team->name, TEAM_MAX);
+	if (bytes == TEAM_MAX && team->name[TEAM_MAX-1] != '\n'){
+
+		clear_buffer();
+		team->name[TEAM_MAX-1] = '\0';
+	}else if (bytes == TEAM_MAX){
+
+		team->name[TEAM_MAX-1] = '\0';
+	}else{
+
+		team->name[bytes-1] = '\0';
+	}
+
+	printf("Enter team password: ");
+	fflush(stdout);
+	// ensure we receive null terminate the string and burn the excess input buffer
+	bytes = read(0, team->pass, TEAM_MAX);
+	if (bytes == TEAM_MAX && team->pass[TEAM_MAX-1] != '\n'){
+
+		clear_buffer();
+		team->pass[TEAM_MAX-1] = '\0';
+	}else if (bytes == TEAM_MAX){
+
+		team->pass[TEAM_MAX-1] = '\0';
+	}else{
+
+		team->pass[bytes-1] = '\0';
+	}
+	
+	team->team_num = rand() % 200;
+	printf("\nLogged in as team: %s\n", team->name);
+	printf("Team number: %d\n",team->team_num);
+
+	fflush(stdout);
+	sleep(1);
+
 	return team;
 }
 
+// display the team name and team number
 void display_team(struct ctfteam *team){
 
 	if (team != NULL){
@@ -51,9 +136,11 @@ void display_team(struct ctfteam *team){
 	}else{
 		printf("Team not set.\n");
 	}
+	fflush(stdout);
 	sleep(1.5);
 }
 
+// delete the team struct, req for the use-after-free vuln
 void delete_team(struct ctfteam *team){
 
 	if (team != NULL){
@@ -62,9 +149,12 @@ void delete_team(struct ctfteam *team){
 	}else{
 		printf("Team not set.\n");
 	}
+	fflush(stdout);
 	sleep(1.5);
 }
 
+// print flag if the user-after-free vuln aligns the string "flag"
+// in the team_num integer field
 void print_flag(struct ctfteam *team){
 
 	if (team != NULL && team->team_num == 0x67616c66){
@@ -82,6 +172,7 @@ void print_flag(struct ctfteam *team){
 		exit(0);
 	}else{
 		printf("You do not have permission to access the flag.\n");
+		fflush(stdout);
 		sleep(1);
 	}
 }
@@ -92,6 +183,7 @@ int main(){
 	struct ctfteam *ctf_team = NULL;
 
 	char selection[8];
+	srand(time(NULL));
 
 	printf("\e[1;1H\e[2J");
 	menu();
@@ -104,10 +196,10 @@ int main(){
 		if (*selection != '\n') clear_buffer();
 		switch (*selection){
 
-			case '1': get_user(ctf_user);
+			case '1': ctf_user = get_user(ctf_user);
 				  break;
 			
-			case '2': get_team(ctf_team);
+			case '2': ctf_team = get_team(ctf_team);
 				  break;
 
 			case '3': display_team(ctf_team);
@@ -123,7 +215,8 @@ int main(){
 				  return 0;
 
 			default : printf("Invalid selection.\n");
-				  sleep(.5);
+				  fflush(stdout);
+				  sleep(1);
 		}
 
 		printf("\e[1;1H\e[2J");
